6e57cfcd6c5e54ff084b445a1639ed3e7b6a954e
AFZl210 <tricktomade@gmail.com>
2024-12-18 20:35:32 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello')
+import puppeteer from "puppeteer";
+
+const meetUrl = 'https://meet.google.com/cjb-caws-iir'
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false, args: ['--disable-blink-features=AutomationControlled', '--use-fake-ui-for-media-stream'] });
+  const page = await browser.newPage();
+
+  await page.setViewport({width: 1080, height: 1024});
+  await page.goto(meetUrl);
+
+
+
+  await page.waitForSelector('input');
+  await page.waitForNetworkIdle();
+  await page.click('input');
+
+  await new Promise((re, res) => {
+    setTimeout(res, 50000)
+  })
+}
+
+main();
