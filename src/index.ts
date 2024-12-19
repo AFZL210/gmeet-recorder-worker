478a1a8b4cc8f2cff10fc9702a503432663fc917
AFZl210 <tricktomade@gmail.com>
2024-12-19 14:08:17 +0530

    join meet

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import puppeteer from "puppeteer";
 
-const meetUrl = 'https://meet.google.com/cjb-caws-iir'
+const meetUrl = 'https://meet.google.com/hys-ipuo-sys'
 
 const main = async () => {
   const browser = await puppeteer.launch({ headless: false, args: ['--disable-blink-features=AutomationControlled', '--use-fake-ui-for-media-stream'] });
   const page = await browser.newPage();
-
   await page.setViewport({width: 1080, height: 1024});
+
+  const clickOnButtonWithText = async (text: string) => {
+    await page.waitForSelector('button');
+  
+    const buttons = await page.$$('button');
+    for (const button of buttons) {
+      const text = await page.evaluate(el => el.textContent?.trim(), button);
+      if (text === 'Ask to join') {
+        await button.click();
+        break;
+      }
+    }
+  }
+
+  const fillInputWithPlaceholder = async (placeholder: string, text: string) => {
+    const inputSelector = `input[placeholder="${placeholder}"]`;
+    await page.waitForSelector(inputSelector);
+    await page.click(inputSelector);
+    await page.type(inputSelector, text);
+  }
+
   await page.goto(meetUrl);
-
-
-
-  await page.waitForSelector('input');
   await page.waitForNetworkIdle();
-  await page.click('input');
+  await fillInputWithPlaceholder('Your name', 'testuser');
+  await clickOnButtonWithText('Ask to join');
+
 
   await new Promise((re, res) => {
     setTimeout(res, 50000)
